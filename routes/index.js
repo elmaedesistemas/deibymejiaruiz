b3eeb18404629e43a625f38b7dbb65553e64a0a9
coffescript <dmejia1204@gmail.com>
2020-01-01 17:04:39 -0600

    create of pages contact, home

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Welcome to my Portfolio' })
+  res.render('index.ejs', { link: 'home' }, { title: 'Welcome to my Portfolio' })
+})
+
+/* GET services. */
+router.get('/services', function (req, res, next) {
+  res.render('services', { title: 'Welcome to my Portfolio' })
+})
+
+/* GET contact. */
+router.get('/contact', function (req, res, next) {
+  res.render('contact.ejs', { link: 'contact' }, { title: 'Welcome to my Portfolio' })
 })
 
 module.exports = router
