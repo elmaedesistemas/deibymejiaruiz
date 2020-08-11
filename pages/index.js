a3790e2f26a06c00361e3b8c8706fb8d730d5da8
Deiby Mejia <dmejia1204@gmail.com>
2020-08-11 10:50:45 -0600

    add google analytics

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,20 @@ export class index extends Component {
     render() {
         return (
             <React.Fragment>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-175213951-1"></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `<!-- Global site tag (gtag.js) - Google Analytics -->
+              <script async src="https://www.googletagmanager.com/gtag/js?id=UA-163332503-1"></script>
+              <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments)}
+                gtag('js', new Date());
+                gtag('config', 'UA-175213951-1');
+              </script>
+              `,
+                    }}
+                />
                 <Navbar />
                 <Banner />
                 <About />
